13cecc7f32f2ee1d2b2a0083cd94819844c5c8ed
Saleryv <seryoginvaleriy@gmail.com>
2023-01-25 20:26:59 +0200

    hw7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import css from './App.module.css';
+import { useSelector } from 'react-redux';
+
+
 export const App = () => {
+  const isLoading = useSelector(state => state.contacts.isLoading)
+  console.log(isLoading)
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.app}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList/>
     </div>
   );
 };
